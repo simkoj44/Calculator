f35ac1e25afe9f3b1f04abe5d5895d19975e97dd
Joe Simko <joseph.simko@protonmail.com>
2023-06-09 12:10:23 -0400

    Build Logic for Clear, Delete, Numbers, Decimal
    Build logic for part of the calculator. Use an array to store values and display accordingly. Handle cases with leading 0s and prevent multiple decimals.

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 // Script
 
-let displayed = 0;
+let displayed = ['0'];
 const display = document.querySelector('#displayed-number');
 display.textContent = displayed;
 
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (displayed[0] == '0') {
+            if (!(displayed.length > 1 && displayed[1] == '.')) {
+                displayed.shift();
+            }
+        }
+        displayed.push(e.target.value);
+        updateDisplayed();
+    });
+});
+
 function clearNumber() {
-    displayed = 0;
-    console.log(displayed);
-    display.textContent = displayed;
+    displayed = ['0'];
+    updateDisplayed();
+}
+
+function deleteNumber() {
+    displayed.pop();
+    updateDisplayed();
+}
+
+function addDecimal() {
+    if (displayed.indexOf('.') == -1 ) {
+        displayed.push('.');
+    }
+    updateDisplayed();
+}
+
+function updateDisplayed() {
+    if (displayed.length == 0) {
+        displayed = ['0'];
+    }
+    display.textContent = displayed.join('');
 }
